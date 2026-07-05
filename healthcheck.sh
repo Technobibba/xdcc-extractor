d9c5995be70f61f3f2f8689633eb89629bf17311
Technobibba <dannyzirkler@gmail.com>
2026-07-05 08:59:08 +0200

    Use binary status command for healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -eu
 
-test -x /usr/local/bin/xdcc-extractor
-test -f /app/config.toml
-test -d /downloads
-test -d /state
-
-if grep -q 'password_file="/config/passwords.txt"' /app/config.toml; then
-    test -d /config
-fi
+/usr/local/bin/xdcc-extractor --status >/dev/null
 
 exit 0
